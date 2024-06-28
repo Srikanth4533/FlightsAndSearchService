7521e144eda73e04a961891da04026c572dd1dd5
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-06-28 10:36:56 +0530

    Completed city repository

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const { PORT } = require("../src/config/serverConfig")
+const CityRepository = require("../src/repository/city-repository")
 
 
 const setupAndStartServer = async() => {
@@ -11,8 +12,13 @@ const setupAndStartServer = async() => {
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`)
+        // console.log(City)
+        const repo = new CityRepository()
+        repo.createCity({ name: "Bengaluru" })
+        repo.deleteCity(1)
+
     })
 }
 
